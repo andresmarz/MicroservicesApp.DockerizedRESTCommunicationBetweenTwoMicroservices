d5ee46643920d6579209fccc3fabbe450c51fb3a
andresmarz <andresmarquezzuleta@gmail.com>
2025-09-12 21:48:36 -0400

    Agregar archivos de proyecto.

---
+++ Services/OrderingService/Ordering.API/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Interfaces;
+using Ordering.Application.Services;
+using Ordering.Application.Services.Orchestration;
+using Ordering.Domain.Interfaces;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.HttpClients;
+using Ordering.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Configure EF Core
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<OrderingDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Dependency Injection
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderOrchestrationService, OrderOrchestrationService>();
+
+
+//Registering an HttpClient
+builder.Services.AddHttpClient<ICatalogServiceHttpClient, CatalogServiceHttpClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["CatalogService:BaseUrl"]!);
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Apply automated migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OrderingDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
